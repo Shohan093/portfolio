e058c807509e9fbfb079dd7e2e4459c7279f9273
Anisujjaman Shohan <anisujjamanshohan093@gmail.com>
2025-05-30 23:17:23 +0600

    Add sklearn SVG icon to public/icons directory

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,14 @@ function App() {
           <ul className="flex gap-6 text-sm">
             <li><a href="#hero" className="hover:text-blue-500">Home</a></li>
             <li><a href="#about" className="hover:text-blue-500">About</a></li>
-            <li><a href="#projects" className="hover:text-blue-500">Projects</a></li>
+            <li><a href="#blogs" className="hover:text-blue-500">Blogs</a></li>
             <li><a href="#contact" className="hover:text-blue-500">Contact</a></li>
           </ul>
         </div>
       </nav>
 
       { /* Hero Section */ }
-      <section id="Hero" className="flex items-center justify-center text-center py-32 px-4">
+      <section id="hero" className="flex items-center justify-center text-center py-32 px-4">
         <div>
           <h2 className="flex items-center justify-center text-center py-32 px-4">Hi, I am Shohan</h2>
           <p className="text-lg max-w-xl mx-auto">
@@ -41,7 +41,11 @@ function App() {
       { /* My Stacks */ }
       <section id="tech-stakcs" className="py-20 px-6 bg-white dark:bg-gray-900">
         <div className="max-w-5xl mx-auto">
-          <h2 className="text-3xl font-bold text-center mb-12">Technologies I Use</h2>
+          <h2 className="text-3xl font-bold text-center mb-6">Technologies I Use</h2>
+          <p className="text-lg leading-relaxed text-gray-700 dark:text-gray-300 text-justify mb-6">
+            Over the years, I have worked with a variety of technologies and tools that have helped me in my journey as a data scientist.
+            Below are some of the key technologies I am proficient in, which I use to solve complex problems and build innovative solutions.
+          </p>
 
           { /* Programming Languages */ }
           <div className="mb-10">
@@ -59,12 +63,45 @@ function App() {
           <div className="mb-10">
             <h3 className="text-xl font-semibold mb-4">Data Manipulation and Visualization</h3>
             <div className="flex flex-wrap gap-8 text-5xl">
-              <i className="devicon-mysql-plain colored" title="MySQL"></i>
               <i className="devicon-numpy-original colored" title="Numpy"></i>
               <i className="devicon-pandas-original colored" title="Pandas"></i>
               <img src="icons/seaborn-1.svg" alt="Seaborn" title="Seaborn" className="w-12 h-12"/>
+              <img src="icons/plotly-express.svg" alt="Plotly-Express" title="Plotly-Express" className="w-12 h-12"/>
               <img src="icons/matplotlib.svg" alt="Matplotlib" title="Matplotlib" className="w-12 h-12"/>
               <img src="icons/scipy.svg" alt="Scipy" title="Scipy" className="w-12 h-12"/>
+              <img src="icons/geopandas_icon.svg" alt="Geopandas" title="Geopandas" className="w-12 h-12"/>
+            </div>
+          </div>
+
+          { /* Database */ }
+          <div className="mb-10">
+            <h3 className="text-xl font-semibold mb-4">Database</h3>
+            <div className="flex flex-wrap gap-8 text-5xl">
+              <i className="devicon-mysql-plain colored" title="MySQL"></i>
+            </div>
+          </div>
+
+          { /* Machine Learning */ }
+          <div className="mb-10">
+            <h3 className="text-xl font-semibold mb-4">ML, DL & NLP</h3>
+            <div className="flex flex-wrap gap-8 text-5xl">
+              <img src="icons/sklearn.svg" alt="Scikit-learn" title="Scikit-Learn" className="h-12 w-12"/>
+              <i className="devicon-pytorch-original colored" title="PyTorch"></i>
+              <i className="devicon-tensorflow-original colored" title="TensorFlow"></i>
+              <img src="icons/keras.svg" alt="Keras" title="Keras" className="w-12 h-12"/>
+            </div>
+          </div>
+
+          {/* Tools */}
+          <div className="mb-10">
+            <h3 className="text-xl font-semibold mb-4">Tools</h3>
+            <div className="flex flex-wrap gap-8 text-5xl">
+              <i className="devicon-linux-plain colored" title="Linux"></i>
+              <i className="devicon-jupyter-plain colored" title="Jupyter Notebook"></i>
+              <i className="devicon-git-plain colored" title="Git"></i>
+              <i className="devicon-github-original colored" title="GitHub"></i>
+              <i className="devicon-latex-original colored" title="LaTeX"></i>
+              <i className="devicon-vscode-plain colored" title="Visual Studio Code"></i>
             </div>
           </div>
         </div>
